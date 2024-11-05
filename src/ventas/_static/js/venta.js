e060d831817faf231fce0be98400bf50f20d6a72
Candelario <candelario.hernandez@induxsoft.net>
2024-11-05 13:36:58 -0600

    Habilitar bitacora

--- src/ventas/_static/js/venta.js
+++ src/ventas/_static/js/venta.js
@@ -1220,3 +1220,23 @@ var venta =
         this.edtProduct(producto,curr_row);
     },
 }
+
+var _bitacora=
+{
+    _url:"",
+    EnableLog(guid,det="") 
+    {
+        if(guid.trim()=="" || this._url.trim()=="")return;
+
+        var webshell=window.top.WebShell;
+        if(!webshell)
+        {
+            console.warn("No se pudo obtener el elemento de webshell");
+            return;
+        }
+
+        var uri=_bitacora._url.replace("@guid",guid).replace("@det",det);
+        console.log(uri)
+        webshell.Panels.Show(webshell.Panels.Const.Right,uri);
+    }
+}
